d4282fa08380079bcc48f42e2074648bac6fdb93
[KsuyS] <[solovevak105@gmail.com]>
2025-07-15 14:32:31 +0300

    fix: правки после мёржа

--- public/js/entity/upgrades.js
+++ public/js/entity/upgrades.js
@@ -3,7 +3,7 @@ export const towerUpgrades = [
         name: 'Урон',
         descriptions: ['+10 урона', '+15 урона', '+20 урона', '+25 урона', '+30 урона'],
         costs: [20, 30, 40, 50, 60],
-        iconSrc: '/images/damage.png',
+        iconSrc: '/images/assets/damage.png',
         applyLevels: [
             (tower) => { tower.damage += 10; },
             (tower) => { tower.damage += 15; },
@@ -16,7 +16,7 @@ export const towerUpgrades = [
         name: 'Радиус',
         descriptions: ['+30 к радиусу', '+50 к радиусу'],
         costs: [50, 80],
-        iconSrc: '/images/radius.png',
+        iconSrc: '/images/assets/radius.png',
         applyLevels: [
             (tower) => { tower.radius += 30; },
             (tower) => { tower.radius += 50; },
@@ -26,7 +26,7 @@ export const towerUpgrades = [
         name: 'Перезарядка',
         descriptions: ['-0,5 сек к перезарядке', '-1 сек к перезарядке'],
         costs: [100, 130],
-        iconSrc: '/images/cooldown.png',
+        iconSrc: '/images/assets/cooldown.png',
         applyLevels: [
             (tower) => { tower.cooldown = Math.max(1, tower.cooldown - 0.5); },
             (tower) => { tower.cooldown = Math.max(1, tower.cooldown - 1); },
