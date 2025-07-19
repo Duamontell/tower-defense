aa5faf1dfbde5026dda2b67fc80739b46498b2b8
Dutell <68645600+Duamontell@users.noreply.github.com>
2025-07-19 13:30:53 +0300

    Добавил главное меню с выбором режима игры
    * feat: Добавил главное меню с выбором режима игры
    
    * fix: Убрал комментарии

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -14,19 +14,56 @@ class GameController extends AbstractController
     #[Route('/menu', name: 'menu')]
     public function menu() :Response
     {
-        return $this->render('menu/menu.html.twig');
+        return $this->render('menu/main-menu.html.twig');
+    }
+
+    #[Route('/select-level', name: 'select-level')]
+    public function selectLevel() :Response
+    {
+        return $this->render('menu/select-level.html.twig');
+    }
+
+    public function singleplayer(Request $request) :Response
+    {
+        $level = (int)$request->get('level');
+        $userId = uniqid();
+
+        return $this->render('game/game.html.twig', [
+            'level' => $level,
+            'userId' => $userId,
+            'gamemode' => 'singleplayer',
+        ]);
     }
 
     #[Route('/game', name: 'game', methods: ['GET'])]
     public function game(Request $request) :Response
     {
         $level = (int)$request->get('level');
-        // Получаем id текущего пользователя
-        $userId = 1;
+        // Получаем id текущего пользователя, пока что генерация
+        $userId = uniqid();
+//      Можно сделать подписку(topic) на имя комнаты: topic=/game/room=$roomId
+//        $roomId = uniqid();
+        $players = [
+            [
+                'userId' => $userId,
+                'config' => '1'
+            ],
+            [
+                'userId' => uniqid(),
+                'config' => '2'
+            ]
+        ];
 
         return $this->render('game/game.html.twig', [
             'level' => $level,
             'userId' => $userId,
+            'gamemode'   => 'multiplayer',
+            'roomConfig' => [
+                'players' => array_map(fn($p) => [
+                    'userId' => $p['userId'],
+                    'config' => $p['config']
+                ], $players),
+            ],
         ]);
     }
 }
