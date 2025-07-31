004c927afc28d2b908b33269399f265119a9f154
Dutell <duamontell@gmail.com>
2025-07-31 12:17:14 +0300

    fix: Убрал функции для дебага

--- public/js/core/game.js
+++ public/js/core/game.js
@@ -213,11 +213,6 @@ function gameLoop(timestamp = 0) {
         drawHyperGameMessage(canvas, ctx, linkText);
     }
 
-    drawPath(14);
-    drawPath(15);
-    drawPath(currentUserId);
-    drawPath(16);
-
     requestAnimationFrame(gameLoop);
 }
 
@@ -324,35 +319,6 @@ if (gameMode === "multiplayer") {
         readyManager.cleanup();
     })();
 }
-
-function drawPath(userId) {
-    world.players.get(userId).waypoints.forEach(waypoint => {
-        ctx.save();
-        ctx.beginPath();
-        ctx.fillStyle = 'black';
-        ctx.arc(waypoint.x, waypoint.y, 7, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke();
-        ctx.restore();
-    })
-}
-
-function legnthCount(waypoints) {
-    let prevWaypoint = waypoints[0];
-    let distance = 0
-    waypoints.forEach(waypoint => {
-        const xDistance = waypoint.x - prevWaypoint.x;
-        const yDistance = waypoint.y - prevWaypoint.y;
-        distance += Math.hypot(xDistance, yDistance);
-        prevWaypoint = waypoint;
-    })
-    console.log(distance);
-}
-
-legnthCount(world.players.get(14).waypoints)
-legnthCount(world.players.get(15).waypoints)
-legnthCount(world.players.get(9).waypoints)
-legnthCount(world.players.get(16).waypoints)
 
 await prepareMultiplayer;
 gameLoop();
